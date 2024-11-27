83a49d2f80097132d2be68229b181fc144a9c587
DEIVIS MACHADO LINO <deivis.lino@alunos.sc.senac.br>
2024-11-26 21:10:06 -0300

    Atualizacao da padronizacao dos codigos de error

--- src/main/java/com/backend/mapeamento/config/SecurityConfig.java
+++ src/main/java/com/backend/mapeamento/config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.backend.mapeamento.config;
 
+import com.backend.mapeamento.exception.CustomAcessDeniedHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -17,10 +21,14 @@ public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
 
+    @Autowired
+    private CustomAcessDeniedHandler customAcessDeniedHandler;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
+                .exceptionHandling(e->e.accessDeniedHandler(customAcessDeniedHandler).authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/login/**").permitAll()
                     .requestMatchers("/login").permitAll()
